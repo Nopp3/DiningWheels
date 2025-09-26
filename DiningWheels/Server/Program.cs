98a7c41b0e7c4132429f9e50fbfd33a42482d0fe
Radosław Rytel-Kuc <radekrytelkuc@gmail.com>
2025-09-26 22:06:26 +0200

    feat: add Dockerized PostgreSQL setup and EF Core configuration
    - add docker-compose with PostgreSQL + pgAdmin
    - configure connection string in appsettings.Development.json
    - update Program.cs with AddPersistance() and AddApplication()
    - add project references for Persistence and Application
    - update .gitignore to exclude bin/, obj/ and EF Core migrations

--- DiningWheels/Server/Program.cs
+++ DiningWheels/Server/Program.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.ResponseCompression;
+using DiningWheels.Application;
+using DiningWheels.Persistance;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+builder.Services.AddPersistance(builder.Configuration).AddApplication();
 
 var app = builder.Build();
 
